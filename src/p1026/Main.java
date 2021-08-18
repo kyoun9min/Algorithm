807694ec6483f32107ceeeeb4fd1bd6fc5211fb5
kyoun9min <rtong77@naver.com>
2021-08-18 21:50:44 +0900

    p1026 수정

--- src/p1026/Main.java
+++ src/p1026/Main.java
@@ -9,23 +9,26 @@ public class Main {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int T = Integer.parseInt(br.readLine());
-		Integer[] a = new Integer[T];
-		Integer[] b = new Integer[T];
+		ArrayList<Integer> listA = new ArrayList<>();
+		ArrayList<Integer> listB = new ArrayList<>();
 		
 		StringTokenizer tokenizerA = new StringTokenizer(br.readLine());
 		StringTokenizer tokenizerB = new StringTokenizer(br.readLine());
 		for (int i = 0; i < T; i++) {
-			a[i] = Integer.parseInt(tokenizerA.nextToken());
-			b[i] = Integer.parseInt(tokenizerB.nextToken());
+			listA.add(Integer.parseInt(tokenizerA.nextToken()));
+			listB.add(Integer.parseInt(tokenizerB.nextToken()));
 		}
 		
-		Arrays.sort(a);
-		Arrays.sort(b, Collections.reverseOrder());
+		Collections.sort(listA);
 		
 		int S = 0;
+		int max = 0;
 		
-		for (int i = 0; i < T; i++)
-			S += a[i] * b[i];
+		for (int i = 0; i < T; i++) {
+			max = Collections.max(listB);
+			S += listA.get(i) * max;
+			listB.remove(Integer.valueOf(max));
+		}
 		
 		System.out.println(S);
 	}
