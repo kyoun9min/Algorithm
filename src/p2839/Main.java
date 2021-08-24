8e80804b61029003a97a931ad34e3e31a34ea921
kyoun9min <rtong77@naver.com>
2021-08-24 20:11:49 +0900

    p2839 dp 풀이 추가

--- src/p2839/Main.java
+++ src/p2839/Main.java
@@ -28,3 +28,32 @@ public class Main {
 	}
 
 }
+
+//// dp를 이용한 풀이
+//public class Main {
+//
+//	public static void main(String[] args) throws IOException, NumberFormatException {
+//
+//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//		int T = Integer.parseInt(br.readLine());
+//		
+//		if(T < 5) {
+//			if(T == 3) System.out.println("1");
+//			else System.out.println("-1");
+//			return;
+//		}
+//
+//		int[] dp = new int[T + 1];
+//		Arrays.fill(dp, 1700);
+//		dp[3] = 1;
+//		dp[5] = 1;
+//		
+//		for(int i = 6; i < T + 1; i++)
+//				dp[i] = Math.min(dp[i - 3], dp[i - 5]) + 1;
+//		if (dp[T] >= 1700)
+//			System.out.println("-1");
+//		else
+//			System.out.println(dp[T]);
+//	}
+//
+//}
