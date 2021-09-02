fdee8bd9a99025e0073050cc56f80ea7f962b1ac
kyoun9min <rtong77@naver.com>
2021-09-02 22:54:36 +0900

    p2468, p2644, p11724, p4963, p1541, p1946

---
+++ src/p1541/Main.java
@@ -0,0 +1,57 @@
+package p1541;
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String[] minus = br.readLine().split("-");
+		int sum = 0;
+		int flag = 0;
+		
+		for (int i = 0; i < minus.length; i++) {
+			String[] plus = minus[i].split("\\+");
+			int temp = 0;
+			for (int j = 0; j < plus.length; j++)
+				temp += Integer.parseInt(plus[j]);
+			if (flag == 0) {
+				sum += temp;
+				flag = 1;
+			}
+			else
+				sum -= temp;
+		}
+		
+		System.out.println(sum);
+	}
+
+}
+
+//public class Main {
+//	
+//	public static void main(String[] args) throws IOException {
+//		
+//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//		StringTokenizer minus = new StringTokenizer(br.readLine(), "-");
+//		int sum = 0;
+//		int flag = 0;
+//		
+//		while (minus.hasMoreTokens()) {
+//			StringTokenizer plus = new StringTokenizer(minus.nextToken(), "+");
+//			int temp = 0;
+//			while (plus.hasMoreTokens()) {
+//				temp += Integer.parseInt(plus.nextToken());
+//			}
+//			if (flag == 0) {
+//				sum += temp;
+//				flag = 1;
+//			}
+//			else
+//				sum -= temp;
+//		}
+//		System.out.println(sum);
+//	}
+//}
---
+++ src/p4963/Main.java
@@ -0,0 +1,84 @@
+package p4963;
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+	static int w;
+	static int h;
+	static int[][] map;
+	static boolean[][] check;
+	static int[] dr = {-1, 1, 0, 0, -1, -1, 1, 1};
+	static int[] dc = {0, 0, -1, 1, -1, 1, -1, 1};
+	static int count;
+	
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		while (true) {
+			StringTokenizer tokenizer = new StringTokenizer(br.readLine());
+			w = Integer.parseInt(tokenizer.nextToken());
+			h = Integer.parseInt(tokenizer.nextToken());
+			if (w == 0 && h == 0)
+				break;
+			map = new int[h][w];
+			check = new boolean[h][w];
+			count = 0;
+			for (int i = 0; i < h; i++) {
+				tokenizer = new StringTokenizer(br.readLine());
+				for (int j = 0; j < w; j++)
+					map[i][j] = Integer.parseInt(tokenizer.nextToken());
+			}
+			
+			for (int i = 0; i < h; i++) {
+				for (int j = 0; j < w; j++)
+					if (map[i][j] == 1 && !check[i][j]) {
+						dfs(i, j);
+//						bfs(i, j);
+						count++;
+					}
+			}
+			bw.write(count + "\n");
+		}
+		bw.flush();
+		bw.close();
+	}
+	
+	public static void dfs(int x, int y) {
+		check[x][y] = true;
+		
+		for (int i = 0; i < 8; i++) {
+			int nx = x + dr[i];
+			int ny = y + dc[i];
+			if (nx >= 0 && ny >= 0 && nx < h && ny < w)
+				if (map[nx][ny] == 1 && !check[nx][ny])
+					dfs(nx, ny);
+		}
+	}
+	
+//	public static void bfs(int x, int y) {
+//		Queue<Integer[]> queue = new LinkedList<>();
+//		queue.offer(new Integer[] {x, y});
+//		
+//		while (!queue.isEmpty()) {
+//			int currX = queue.peek()[0];
+//			int currY = queue.peek()[1];
+//			queue.poll();
+//			if (!check[currX][currY]) {
+//				check[currX][currY] = true;
+//				for (int i = 0; i < 8; i++) {
+//					int nx = currX + dr[i];
+//					int ny = currY + dc[i];
+//					if (nx >= 0 && ny >= 0 && nx < h && ny < w)
+//						if (map[nx][ny] == 1)
+//							queue.offer(new Integer[] {nx, ny});
+//				}
+//			}
+//			
+//		}
+//	}
+
+}
